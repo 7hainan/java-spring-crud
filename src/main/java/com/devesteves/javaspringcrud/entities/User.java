4a053ef2a8e411e70725599dff2bce61e2918134
Thainan Esteves <thainan239@gmail.com>
2023-06-27 18:00:02 -0300

    create Order entitie and some layers classes about Order

--- src/main/java/com/devesteves/javaspringcrud/entities/User.java
+++ src/main/java/com/devesteves/javaspringcrud/entities/User.java
@@ -1,9 +1,12 @@
 package com.devesteves.javaspringcrud.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -19,6 +22,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+    @JsonIgnore
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User(){
     }
@@ -65,6 +71,10 @@ public class User implements Serializable {
 
     public String getPassword() {
         return password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     public void setPassword(String password) {
