8615490c8f363424ce718f50d2787669141e2fa0
Thainan Esteves <thainan239@gmail.com>
2023-06-27 15:43:45 -0300

    starting a new repo and create response entitie User

---
+++ src/main/java/com/devesteves/javaspringcrud/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.devesteves.javaspringcrud.resources;
+
+import com.devesteves.javaspringcrud.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+
+        User user = new User(1L,"Nathan","nathan@gmail.com","999999999","123456");
+
+        return ResponseEntity.ok().body(user);
+    }
+}
---
+++ src/test/java/com/devesteves/javaspringcrud/JavaSpringCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devesteves.javaspringcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
